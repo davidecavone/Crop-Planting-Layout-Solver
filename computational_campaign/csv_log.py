c3842fb80b248ffc22db1d9427f07ec3fb776eb5
Davide Cavone <34422118+davidecavone@users.noreply.github.com>
2026-09-07 10:44:35 +0200

    Implement CSV data analysis and error handling
    Add analyze function to aggregate CSV data and handle file errors.

--- computational_campaign/csv_log.py
+++ computational_campaign/csv_log.py
@@ -55,3 +55,27 @@ def finalize_csv(csv_file, csv_path):
     df['maximal'] = df.groupby('istanza_pk')['z'].transform('max')
     df.to_csv(csv_path, index=False)
     print(f"CSV finale salvato in: {csv_path}")
+
+# Aggregate computational campaign CSV data in a table
+def analyze(csv_path):
+    try:
+        df = pd.read_csv(csv_path)
+    except FileNotFoundError:
+        print(f"Errore: file '{csv_path}' non trovato.")
+        sys.exit(1)
+
+    df['60s']  = df['time_first_feasible'] <= 60
+    df['120s'] = df['time_first_feasible'] <= 120
+    df['240s'] = df['time_first_feasible'] <= 240
+    df['480s'] = df['time_first_feasible'] <= 480
+
+    table = df.groupby(['H', 'K', 'N']).agg(
+        count=('instance', 'count'),
+        s60=('60s', 'sum'),
+        s120=('120s', 'sum'),
+        s240=('240s', 'sum'),
+        s480=('480s', 'sum')
+    ).reset_index()
+
+    timestamp = df['time_first_feasible'].name
+    table.to_csv(f'results/aggregated_{timestamp}.csv', index=False)
