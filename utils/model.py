64124eada9ac5990671f0b53986cf83e9a088c12
Davide Cavone <34422118+davidecavone@users.noreply.github.com>
2026-09-07 10:11:11 +0200

    Moved CP-SAT solver status mapping and callback into the benchmark script

--- utils/model.py
+++ utils/model.py
@@ -15,28 +15,6 @@ def overlap_length(model, start_a, end_a, start_b, end_b, name, DIM_STRIP):
     model.add_max_equality(overlap, [diff, 0])
     return overlap
 
-# Map CP-SAT solver status
-def map_status(cp_status):
-    if cp_status == cp.OPTIMAL:
-        return 1
-    elif cp_status == cp.FEASIBLE:
-        return 0
-    else:
-        return -1
-
-# Callback for the first found solution
-class FirstSolutionCallback(cp.CpSolverSolutionCallback):
-    def __init__(self):
-        cp.CpSolverSolutionCallback.__init__(self)
-        self._first_solution_time = None
-
-    def on_solution_callback(self):
-        if self._first_solution_time is None:
-            self._first_solution_time = self.wall_time
-
-    def first_solution_time(self):
-        return self._first_solution_time
-
 # Create and solve the model
 def build_and_solve(K, M, H, a, o, c_min, c_max, d,
                     constraint_mode, allelopathy_threshold,
