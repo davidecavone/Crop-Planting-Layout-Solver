70f46cae6596fa38a35b412e2b58fead0c896895
Davide Cavone <34422118+davidecavone@users.noreply.github.com>
2026-09-07 10:12:11 +0200

    Add benchmark utility with solution callback

---
+++ utils/benchmark.py
@@ -0,0 +1,20 @@
+def map_status(cp_status):
+    if cp_status == cp.OPTIMAL:
+        return 1
+    elif cp_status == cp.FEASIBLE:
+        return 0
+    else:
+        return -1
+
+# Callback for the first found solution
+class FirstSolutionCallback(cp.CpSolverSolutionCallback):
+    def __init__(self):
+        cp.CpSolverSolutionCallback.__init__(self)
+        self._first_solution_time = None
+
+    def on_solution_callback(self):
+        if self._first_solution_time is None:
+            self._first_solution_time = self.wall_time
+
+    def first_solution_time(self):
+        return self._first_solution_time
