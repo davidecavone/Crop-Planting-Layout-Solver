82995bb325b14e844bc7421f853b1bcfac05e2cf
Davide Cavone <34422118+davidecavone@users.noreply.github.com>
2026-09-09 00:11:13 +0200

    Remove analyze function from csv_log.py
    Removed the analyze function that aggregated CSV data and its related code and added results folder existence check

--- computational_campaign/csv_log.py
+++ computational_campaign/csv_log.py
@@ -2,12 +2,15 @@ from datetime import datetime
 import csv
 import pandas as pd
 
-from csv_log import *
 
 # Creates a CSV file for the computational campaign
 def init_csv(base_dir):
     timestamp  = datetime.now().strftime('%d-%m-%Y_%H-%M-%S')
     csv_path   = base_dir / 'results' / f'{timestamp}.csv'
+
+    # Ensure the 'results' folder exists
+    csv_path.parent.mkdir(parents=True, exist_ok=True)
+    
     csv_file   = open(csv_path, 'w', newline='')
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow([
@@ -57,27 +60,6 @@ def finalize_csv(csv_file, csv_path):
     df['maximal'] = df.groupby('istanza_pk')['z'].transform('max')
     df.to_csv(csv_path, index=False)
     print(f"CSV file saved in: {csv_path}")
-
-# Aggregate computational campaign CSV data in a table
-def analyze(csv_path):
-    try:
-        df = pd.read_csv(csv_path)
-    except FileNotFoundError:
-        print(f"Error: fil '{csv_path}' not found.")
-        sys.exit(1)
-
-    df['60s']  = df['time_first_feasible'] <= 60
-    df['120s'] = df['time_first_feasible'] <= 120
-    df['240s'] = df['time_first_feasible'] <= 240
-    df['480s'] = df['time_first_feasible'] <= 480
-
-    table = df.groupby(['H', 'K', 'N']).agg(
-        count=('instance', 'count'),
-        s60=('60s', 'sum'),
-        s120=('120s', 'sum'),
-        s240=('240s', 'sum'),
-        s480=('480s', 'sum')
-    ).reset_index()
 
     timestamp = df['time_first_feasible'].name
     table.to_csv(f'results/aggregated_{timestamp}.csv', index=False)
